ca51f787035510cbe9b6bcf2a1891589110927ea
NAILY KHAIRIYA <nailykhairiya@gmail.com>
2022-12-22 13:14:41 +0700

    footer

--- footer.php
+++ footer.php
@@ -24,61 +24,40 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-3">
+        <div class="col-lg-5">
           <div class="footer-widget">
-            <h4>Contact Us</h4>
-            <p>Rio de Janeiro - RJ, 22795-008, Brazil</p>
+            <h4>Kontak Kami</h4>
+            <p>Wonocolo, Jawa Timur</p>
             <p><a href="#">010-020-0340</a></p>
-            <p><a href="#">info@company.co</a></p>
+            <p><a href="#">dinas_kelautan@indo.com</a></p>
           </div>
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-4">
           <div class="footer-widget">
             <h4>About Us</h4>
             <ul>
               <li><a href="#">Home</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Testimonials</a></li>
-              <li><a href="#">Pricing</a></li>
+              <li><a href="#">Daftar Ujian</a></li>
+              <li><a href="#">Upload Berkas</a></li>
+              <li><a href="#">Cetak Kartu</a></li>
+              <li><a href="#">Ujian</a></li>
             </ul>
-            <ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Testimonials</a></li>
-              <li><a href="#">Pricing</a></li>
-            </ul>
+     
           </div>
         </div>
+
         <div class="col-lg-3">
           <div class="footer-widget">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><a href="#">Free Apps</a></li>
-              <li><a href="#">App Engine</a></li>
-              <li><a href="#">Programming</a></li>
-              <li><a href="#">Development</a></li>
-              <li><a href="#">App News</a></li>
-            </ul>
-            <ul>
-              <li><a href="#">App Dev Team</a></li>
-              <li><a href="#">Digital Web</a></li>
-              <li><a href="#">Normal Apps</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3">
-          <div class="footer-widget">
-            <h4>About Our Company</h4>
+            <h4>Tentang Dinas</h4>
             <div class="logo">
               <img src="assets/images/logoo.png" alt="">
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            <p>Silakan melakukan registrasi untuk pendaftaran ujian</p>
           </div>
         </div>
         <div class="col-lg-12">
           <div class="copyright-text">
-            <p>Copyright © 2022 Chain App Dev Company. All Rights Reserved. 
-          <br>Design: <a href="https://templatemo.com/" target="_blank" title="css templates">TemplateMo</a></p>
+            <p>Copyright © Dinas Kelautan dan Perikanan</p>
           </div>
         </div>
       </div>
